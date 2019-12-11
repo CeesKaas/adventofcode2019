86d06b150734b9a7d62ba1a43e0d1fea79990fe6
Cees Kaas <cees.kaas@gmail.com>
2019-12-12 00:15:20 +0100

    Cleaned unused code from day 10

--- AdventOfCode2019/Day10/Asteroid.cs
+++ AdventOfCode2019/Day10/Asteroid.cs
@@ -16,61 +16,6 @@ namespace Day10
         }
         public Point Location { get; }
 
-
-        public ICollection<Point> GetPointsBetween(Point other)
-        {
-            return GetPointsBetween(Location, other);
-        }
-        public static ICollection<Point> GetPointsBetween(Point a, Point b)
-        {
-            List<Point> points = new List<Point>();
-            if (a.X == b.X)
-            {
-                var max = Math.Max(a.Y, b.Y);
-                var min = Math.Min(a.Y, b.Y);
-                for (int i = min + 1; i < max; i++)
-                {
-                    points.Add(new Point(a.X, i));
-                }
-                return points;
-            }
-            if (a.Y == b.Y)
-            {
-                var max = Math.Max(a.X, b.X);
-                var min = Math.Min(a.X, b.X);
-                for (int i = min + 1; i < max; i++)
-                {
-                    points.Add(new Point(i, a.Y));
-                }
-                return points;
-            }
-            var leftMost = a.X < b.X ? a : b;
-            var rightMost = a.X > b.X ? a : b;
-
-            var slope = (leftMost.Y - (double)rightMost.Y) / (leftMost.X - (double)rightMost.X);
-            var offset = leftMost.Y - (leftMost.X * slope);
-
-            for (int x = leftMost.X + 1; x < rightMost.X; x++)
-            {
-                double yDouble = (x * slope) + offset;
-                int y = (int)yDouble;
-                if (Math.Abs(y - yDouble) < 0.01)
-                {
-                    points.Add(new Point(x, y));
-                }
-            }
-            return points;
-        }
-        [Test]
-        public void GetPointsBetweenTest()
-        {
-            var p1 = GetPointsBetween(new Point(0, 3), new Point(3, 0));
-            Assert.That(p1, Is.EquivalentTo(new[] { new Point(1, 2), new Point(2, 1) }));
-
-            var p2 = GetPointsBetween(new Point(0, 3), new Point(4, 1));
-            Assert.That(p2, Is.EquivalentTo(new[] { new Point(2, 2) }));
-        }
-
         public double GetAngleTo(Asteroid other)
         {
             var a = Location;
@@ -88,23 +33,6 @@ namespace Day10
             var dX = Math.Max(a.X, b.X) - Math.Min(a.X, b.X);
             var dY = Math.Max(a.Y, b.Y) - Math.Min(a.Y, b.Y);
             return Math.Sqrt(dX * dX + dY * dY);
-        }
-
-        public double GetSlopeTo(Asteroid other)
-        {
-            var a = Location;
-            var b = other.Location;
-            if (a.X == b.X)
-            {
-                return a.Y < b.Y ? double.PositiveInfinity : double.NegativeInfinity;
-            }
-            if (a.Y == b.Y)
-            {
-                return a.X < b.X ? 0.0 : double.MaxValue;
-            }
-
-            var slope = (a.Y - (double)b.Y) / (a.X - (double)b.X);
-            return slope;
         }
     }
     public class Point : IEquatable<Point>
